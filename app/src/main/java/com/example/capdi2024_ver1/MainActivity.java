c8ee54c9eee1702d2248d0bd41c4cf26c5c6775b
kimjunho <99apollo@naver.com>
2024-04-11 20:43:40 +0900

    test1

--- app/src/main/java/com/example/capdi2024_ver1/MainActivity.java
+++ app/src/main/java/com/example/capdi2024_ver1/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.capdi2024_ver1;
 
+import static android.widget.Toast.makeText;
+import static java.sql.Types.NULL;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button loginbutton = findViewById(R.id.login_button);
+        TextView idtext= findViewById(R.id.id_input_text);
+        EditText passtext= findViewById(R.id.password_input_text);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CharSequence idin = idtext.getText();
+                CharSequence pwin = passtext.getText();
+                if (idin.length() == 0 || pwin.length() == 0) {
+                    makeText(getApplicationContext(), "입력해주세요.", Toast.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(MainActivity.this, ClientPage.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 }
