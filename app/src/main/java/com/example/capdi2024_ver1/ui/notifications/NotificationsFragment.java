915d695a05efc75916580b45d57db1ebbb0c73ab
kimjunho <99apollo@naver.com>
2024-05-07 14:56:25 +0900

    fragment info 버전 수정

--- app/src/main/java/com/example/capdi2024_ver1/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/capdi2024_ver1/ui/notifications/NotificationsFragment.java
@@ -1,6 +1,9 @@
 package com.example.capdi2024_ver1.ui.notifications;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,14 +12,21 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.capdi2024_ver1.R;
+import com.example.capdi2024_ver1.SharedViewModel;
 import com.example.capdi2024_ver1.databinding.FragmentNotificationsBinding;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private SharedViewModel sharedViewModel;
+    private String userId;  // 전달받은 userId를 저장할 필드
+    private DatabaseReference itemListRef;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -29,6 +39,15 @@ public class NotificationsFragment extends Fragment {
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
 
+        itemListRef = FirebaseDatabase.getInstance().getReference("item_list");
+        sharedViewModel = new ViewModelProvider(requireActivity()).get(SharedViewModel.class);
+        sharedViewModel.getUserId().observe(getViewLifecycleOwner(), new Observer<String>() {  // `LiveData` 관찰
+            @Override
+            public void onChanged(String newUserId) {
+                userId=newUserId;
+                Log.d(TAG, "idin: " + userId);
+            }
+        });
         return root;
     }
 
